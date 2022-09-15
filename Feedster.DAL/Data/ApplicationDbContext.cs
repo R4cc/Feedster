ece831df98f319e838d407fd464923917b927561
Niklas L <niklas.lerchenmueller@protonmail.com>
2022-09-15 08:53:11 +0200

    Update gitignore

--- Feedster.DAL/Data/ApplicationDbContext.cs
+++ Feedster.DAL/Data/ApplicationDbContext.cs
@@ -52,7 +52,6 @@ namespace Feedster.DAL.Data
                 Groups = new List<Group>() 
             });
             
-            
             feed.HasData(new Feed()
             {
                 FeedId = 3,
