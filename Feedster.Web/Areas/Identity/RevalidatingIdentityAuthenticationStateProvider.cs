c5702d79f0f6fb716f0bd509bf37d0cf1fd12a67
R4cc <48733309+R4cc@users.noreply.github.com>
2025-08-13 14:42:57 +0200

    Use async scope for identity revalidation

--- Feedster.Web/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
+++ Feedster.Web/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using System.Security.Claims;
 
@@ -28,24 +29,10 @@ namespace Feedster.Web.Areas.Identity
         protected override async Task<bool> ValidateAuthenticationStateAsync(
             AuthenticationState authenticationState, CancellationToken cancellationToken)
         {
-            // Get the user manager from a new scope to ensure it fetches fresh data
-            var scope = _scopeFactory.CreateScope();
-            try
-            {
-                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<TUser>>();
-                return await ValidateSecurityStampAsync(userManager, authenticationState.User);
-            }
-            finally
-            {
-                if (scope is IAsyncDisposable asyncDisposable)
-                {
-                    await asyncDisposable.DisposeAsync();
-                }
-                else
-                {
-                    scope.Dispose();
-                }
-            }
+            // Use an async scope to ensure fresh data without manual disposal
+            await using var scope = _scopeFactory.CreateAsyncScope();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<TUser>>();
+            return await ValidateSecurityStampAsync(userManager, authenticationState.User);
         }
 
         private async Task<bool> ValidateSecurityStampAsync(UserManager<TUser> userManager, ClaimsPrincipal principal)
